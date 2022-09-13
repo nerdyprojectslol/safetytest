38fac0e9992ca4f80a8108b5afaa11760649064b
LilGamerPlayz <92957880+LilGamerPlayz@users.noreply.github.com>
2022-09-12 23:38:13 -0700

    Minor Update 4.21
    safetytest.html
    - Added Spacing
    
    test.js
    -Reworded Questions

--- libraries/test.js
+++ libraries/test.js
@@ -9,7 +9,7 @@ let PossibleQuestionsCount = 16;
 PossibleQuestions[0] = "What types of things are MSDSs used for?";
 PossibleQuestions[1] = "What is the unabbreviated form of PPE?";
 PossibleQuestions[2] = "In what situations do you need proper ventilation?";
-PossibleQuestions[3] = "Which of the following possible outcomes of not following safety protocol?";
+PossibleQuestions[3] = "Which of the following are possible outcomes of not following safety protocol?";
 PossibleQuestions[4] = "In what situations do you need to wear a filter mask?";
 PossibleQuestions[5] = "What are the most important attributes for safety?";
 PossibleQuestions[6] = "Which of the following should always be available?";
@@ -20,11 +20,11 @@ PossibleQuestions[10] = "What is the job of the Safety Captain?";
 PossibleQuestions[11] = "When do you NOT use a potentially dangerous tool?";
 PossibleQuestions[12] = "Which of the following are proper PPE for usage of an angle grinder?";
 PossibleQuestions[13] = "Which of the following are adequate PPE for normal robot assembly?";
-PossibleQuestions[14] = "What items go in the Safety Binder?";
+PossibleQuestions[14] = "What items should go in the Safety Binder?";
 PossibleQuestions[15] = "What shouldn't be done for proper electicity use?";
 PossibleQuestions[16] = "Which of the following items should always be available?";
 PossibleQuestions[17] = "What items should be noted on a MSDS?";
-PossibleQuestions[18] = "What person should you go to for safety guidance?";
+PossibleQuestions[18] = "What person should you go to for safety guidance/instruction?";
 
 //Answers to each question held
 
