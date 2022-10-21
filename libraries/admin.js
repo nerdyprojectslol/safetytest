f2367b29438369952513fc0b8c1b878312d98ed4
LilGamerPlayz <92957880+LilGamerPlayz@users.noreply.github.com>
2022-10-21 16:52:31 -0700

    Major Update 6.3
    index.html
     - Updated Date
    
    README.md
     - Updated Version
    
    data.json
     - Changed some question values
    
    index.html (MainSafetyTest)
     - Cleaned code
    
    admin.js
     - Deleted
    
    adminpass.json
     - Deleted

--- libraries/admin.js
+++
@@ -1,7 +0,0 @@
-        //Fetching questions.json file as promise to resolve
-        fetch("/libraries/adminpass.json")
-        .then(data => adminpass.json())
-        .then(data => {
-            datavar = data.PossibleQuestions;
-
-            });
